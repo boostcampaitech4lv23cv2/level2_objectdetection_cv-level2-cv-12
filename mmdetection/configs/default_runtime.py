38327eb77e6da3387042f9add890416cbd2897b7
Ui-Seok <kasasima18@gmail.com>
2022-11-23 10:14:18 +0000

    edit gitignore & wandb custom code

--- mmdetection/configs/default_runtime.py
+++ mmdetection/configs/default_runtime.py
@@ -1,21 +1,7 @@
-work_dir = './work_dirs/eok' # save path 설정
 checkpoint_config = dict(interval=1) # 1에폭에 한번 저장
 
 # yapf:disable
-log_config = dict(
-    interval=100,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='MMDetWandbHook',
-            init_kwargs={'project': 'Object Detection',
-                         'name': 'seok-test',
-                         'entity': 'cv12'},
-            interval=100, # Logging interval
-            log_checkpoint=True, # Save the checkpoint at every checkpoint interval as W&B Artifacts
-            log_checkpoint_metadata=True,  # Log the evaluation metrics computed on the validation data with the checkpoint
-            num_eval_images=100,
-            bbox_score_thr=0.3), # The number of validation images to be logged.
-    ])
+
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
 
