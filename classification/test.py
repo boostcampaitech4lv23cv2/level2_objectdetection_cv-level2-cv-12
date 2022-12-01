a0efca1e9c73322d512f57e32c8c1b469b494cc5
paststar <ladycar96@gmail.com>
2022-12-01 07:31:47 +0000

    finish test.py

--- classification/test.py
+++ classification/test.py
@@ -38,30 +38,39 @@ if __name__ == "__main__":
 
     parser.add_argument("--backbone_name", type=str, default="efficientnet_b0")
     parser.add_argument("--checkpoint_path", type=str, default="work_dirs/classification/efficientnet_b0/best_model.pth")
-    parser.add_argument("--result_csv", type=str, default="work_dirs/classification/efficientnet_b0/test_class.csv")
-
+    parser.add_argument("--result_csv", type=str, default="work_dirs/classification/efficientnet_b0/val_class.csv")
+    #parser.add_argument("--result_csv", type=str, default="work_dirs/classification/efficientnet_b0/val_class.csv")
 
     args = parser.parse_args()
     set_seed(args.seed)
+    use_cuda = False
+    device = torch.device("cuda" if use_cuda else "cpu")
 
     test_transform = build_transform(args=args, phase="test")
-    #test_dataset = CustomDataset(gt_path='/opt/ml/dataset/kfold/seed41/val_4.json', transform=test_transform)
-    test_dataset = CustomDataset(gt_path='/opt/ml/dataset/test.json', transform=test_transform, phase="test")
+    test_dataset = CustomDataset(gt_path='/opt/ml/dataset/kfold/seed41/val_4.json', transform=test_transform, phase="test")
+    #test_dataset = CustomDataset(gt_path='/opt/ml/dataset/test.json', transform=test_transform, phase="test")
     test_loader = DataLoader(test_dataset,batch_size=args.batch_size,shuffle=False,num_workers=args.n_workers)
 
-    model = CustomModel(args).cuda()
-    checkpoint = torch.load(args.checkpoint_path)['state_dict']
+    model = CustomModel(args).to(device)
+    checkpoint = torch.load(args.checkpoint_path,map_location=device)['model']
+    #print(checkpoint.keys())
     model.load_state_dict(checkpoint)
 
     model.eval()
-    submission = pd.DataFrame()
-    # submission['PredictionString'] = prediction_strings
-    # submission['image_id'] = file_names
+    file_names = []
+    prediction_strings = []
     preds = torch.tensor([])
+    
     with torch.no_grad():
-        for img in tqdm(test_loader):
-            img,image_info = img.cuda(),image_info
+        for img,image_info in tqdm(test_loader):
+            img,image_info = img.to(device), image_info
+            file_names.extend(image_info)
             pred = model(img).sigmoid()
             preds = torch.cat((preds, pred.cpu()))
-    
+        
+    preds = preds.numpy()
+    preds = [" ".join(map(str,i)) for i in preds]
+    submission = pd.DataFrame()
+    submission['image_id'] = file_names
+    submission['classe_str'] = preds
     submission.to_csv(args.result_csv, index=None)
