c6924ee2c55b5efebe10d37cf4cea81e97dc2d50
wowns1484 <wowns1484@naver.com>
2022-12-01 07:53:28 +0000

    modify asgs feature

--- classification/model.py
+++ classification/model.py
@@ -9,19 +9,11 @@ import timm
 class CustomModel(nn.Module):
     def __init__(self, args=None):
         super(CustomModel, self).__init__()
-        #num_classes = 11
+
         num_classes = 10
         self.backbone = timm.create_model(
             args.backbone_name, pretrained=True, num_classes=num_classes
         )
-        # if args.backbone_name == "resnet50":
-        #     self.backbone = timm.create_model(
-        #         "resnet50", pretrained=True, num_classes=num_classes
-        #     )
-        # elif args.backbone_name == 'efficientnetb0':
-        #     self.backbone = timm.create_model(
-        #         "resnet50", pretrained=True, num_classes=num_classes
-        #     )
 
     def forward(self, x):
         x = self.backbone(x)
--- classification/test.py
+++ classification/test.py
@@ -1,34 +1,17 @@
-import os
 import argparse
-import random
-from datetime import datetime
-from collections import defaultdict
 
-import wandb
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
-#from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, WeightedRandomSampler
-from torch.optim import Adam, SGD
-from torch.optim.lr_scheduler import StepLR, CosineAnnealingLR
-from torch.nn import BCEWithLogitsLoss
-import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 from dataset import CustomDataset
 from model import CustomModel
 from utils import *
-
-# from timm.scheduler.step_lr import StepLRScheduler
-#from metric import accuracy, macro_f1
-
-#from process import train, validation
-#from loss import MultitaskLoss, create_criterion
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -37,15 +20,14 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=2)
 
     parser.add_argument("--backbone_name", type=str, default="efficientnet_b0")
-    parser.add_argument("--checkpoint_path", type=str, default="work_dirs/classification/efficientnet_b0/best_model.pth")
-    parser.add_argument("--result_csv", type=str, default="work_dirs/classification/efficientnet_b0/test_class.csv")
+    parser.add_argument("--checkpoint_path", type=str, default="/opt/ml/level2_objectdetection_cv-level2-cv-12/classification/work_dirs/classification/efficientnet_b2/best_model.pth")
+    parser.add_argument("--result_csv", type=str, default="/opt/ml/level2_objectdetection_cv-level2-cv-12/classification/work_dirs/classification/efficientnet_b2/test_class.csv")
 
-
+    parser.add_argument("--image_scale", type=tuple, default=(1024, 1024))
     args = parser.parse_args()
     set_seed(args.seed)
 
-    test_transform = build_transform(args=args, phase="test")
-    #test_dataset = CustomDataset(gt_path='/opt/ml/dataset/kfold/seed41/val_4.json', transform=test_transform)
+    test_transform = build_transform(args=args, phase="test", image_scale=args.image_scale)
     test_dataset = CustomDataset(gt_path='/opt/ml/dataset/test.json', transform=test_transform, phase="test")
     test_loader = DataLoader(test_dataset,batch_size=args.batch_size,shuffle=False,num_workers=args.n_workers)
 
